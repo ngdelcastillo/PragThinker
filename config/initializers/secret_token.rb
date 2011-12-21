6871c9a2622c42759523b7e3f1bc7db0ecc00033
Noel del Castillo <ngdelcastillo@gmail.com>
2011-12-21 22:21:51 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PragThinker::Application.config.secret_token = '6f8d8c3cd51ce1af6a24721b0c8b32255bc027f84cc39700e71e7d992c53439121d0415558897c807ee326e8be0981937ff9d5875271b040fcce5e2de1963b1f'
